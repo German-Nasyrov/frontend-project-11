a8f804593f9e8019ba1aeaa7dbaeb324263d6136
oggera <german.nasyrov.95@mail.ru>
2023-02-05 20:37:43 +0300

    create i18next instance

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './styles.scss';
 import 'bootstrap';
+import i18n from 'i18next';
+import resources from './locales/index.js';
+import app from './app.js';
 
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', () => {
+    const i18next = i18n.createInstance();
+    i18next.init({
+      lng: 'ru',
+      debug: true,
+      resources,
+    })
+      .then(() => app(i18next))
+      .catch((e) => e.message);
+  });
+
+
