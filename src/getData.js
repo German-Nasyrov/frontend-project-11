ced66023acc60da7af1297ff20094edb1144d184
og_gera <german.nasyrov.95@mail.ru>
2023-02-16 15:29:50 +0300

    fix getData.js

--- src/getData.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const getData = (url) => axios
-  .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
-
-export default getData;
