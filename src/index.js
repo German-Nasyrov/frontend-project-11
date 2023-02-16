e4c810891c8938ec56330d702f2ebac419dec4e7
og_gera <german.nasyrov.95@mail.ru>
2023-02-16 15:29:29 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import './scss/styles.scss';
 import 'bootstrap';
-import i18n from 'i18next';
-import resources from './locales/index.js';
-import app from './app.js';
+import runApp from './app.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const i18next = i18n.createInstance();
-  i18next.init({
-    lng: 'ru',
-    debug: true,
-    resources,
-  })
-    .then(() => app(i18next))
-    .catch((error) => error.message);
-});
+runApp();
