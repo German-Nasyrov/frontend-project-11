1c2430419f1990001459c806338702d78bd5dd29
og_gera <german.nasyrov.95@mail.ru>
2023-02-16 15:29:13 +0300

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-param-reassign */
 import onChange from 'on-change';
 import uniqueId from 'lodash/uniqueId.js';
+import i18next from 'i18next';
+import axios from 'axios';
 import render from './render.js';
 import validate from './validator.js';
-import getData from './getData.js';
 import parse from './parser.js';
+import resources from './locales/index.js';
 
-export default (i18n) => {
+const runApp = () => {
   const htmlElements = {
     form: document.querySelector('form'),
     input: document.querySelector('#url-input'),
@@ -25,21 +27,32 @@ export default (i18n) => {
     visitedPostsID: [],
   };
 
-  const watchedState = onChange(state, render(state, htmlElements, i18n));
+  const i18nInstance = i18next.createInstance();
+  i18nInstance.init({ lng: 'ru', debug: true, resources });
+
+  const watchedState = onChange(state, render(state, htmlElements, i18nInstance));
 
   const addNewRssElement = (parsedRss, link) => {
     const { feed, posts } = parsedRss;
     feed.id = uniqueId();
     feed.feedLink = link;
-    watchedState.feeds.push(feed);
-    posts.forEach((post) => {
+    watchedState.feeds.unshift(feed);
+    posts.map((post) => {
       const { postTitle, postDescription, postLink } = post;
       const postID = uniqueId();
       const feedID = feed.id;
-      watchedState.posts.push({
+      return watchedState.posts.unshift({
         postTitle, postDescription, postLink, postID, feedID,
       });
     });
+  };
+
+  const getData = (url) => {
+    const proxy = 'https://allorigins.hexlet.app/get';
+    const urlWithProxy = new URL(proxy);
+    urlWithProxy.searchParams.set('disableCache', 'true');
+    urlWithProxy.searchParams.set('url', url);
+    return axios.get(urlWithProxy);
   };
 
   const updateRssElement = () => {
@@ -47,21 +60,23 @@ export default (i18n) => {
     const promises = state.rssLinks.map((url) => {
       getData(url)
         .then((rss) => {
-          const newFeed = state.feeds.find((feed) => feed.feedLink === url);
-          const { feed, posts } = parse(rss.data.contents);
-          feed.id = newFeed.id;
+          const existingFeed = state.feeds.find((feed) => feed.feedLink === url);
+          const { posts } = parse(rss.data.contents);
           const newPosts = posts.filter((post) => {
             const collOfPostsLinks = state.posts.map((postInState) => postInState.postLink);
             return !collOfPostsLinks.includes(post.postLink);
           });
           if (newPosts.length === 0) return;
-          newPosts.forEach((post) => {
+          newPosts.map((post) => {
             post.postID = uniqueId();
-            post.feedID = feed.id;
+            post.feedID = existingFeed.id;
+            return (post.postID, post.feedID);
           });
-          watchedState.posts = [...state.posts, ...newPosts];
+          watchedState.posts.push(...newPosts);
         })
-        .catch((error) => { throw new Error(`Ошибка при обновлении фида: ${url}`, error); });
+        .catch((error) => {
+          throw new Error(`${i18nInstance.t('feedback.feedUpdateError')}${url}`, error);
+        });
       return state;
     });
     Promise.all(promises)
@@ -101,3 +116,5 @@ export default (i18n) => {
 
   updateRssElement();
 };
+
+export default runApp;
