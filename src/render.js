8249f03c6bc098bb6877de8485a0c711b8c51f27
og_gera <german.nasyrov.95@mail.ru>
2023-02-16 15:28:52 +0300

    fix render.js

--- src/render.js
+++ src/render.js
@@ -1,4 +1,4 @@
-const renderFormFillingSuccess = (htmlElements, i18n) => {
+const renderFormFillingSuccess = (htmlElements, i18next) => {
   const {
     form, input, submit, feedback,
   } = htmlElements;
@@ -6,12 +6,12 @@ const renderFormFillingSuccess = (htmlElements, i18n) => {
   input.classList.remove('is-invalid');
   feedback.classList.remove('text-danger');
   feedback.classList.add('text-success');
-  feedback.textContent = i18n.t('feedback.success');
+  feedback.textContent = i18next.t('feedback.success');
   form.reset();
   input.focus();
 };
 
-const renderFormFillingError = (state, htmlElements, i18n) => {
+const renderFormFillingError = (state, htmlElements, i18next) => {
   const { input, submit, feedback } = htmlElements;
   submit.disabled = false;
   input.classList.add('is-invalid');
@@ -19,26 +19,26 @@ const renderFormFillingError = (state, htmlElements, i18n) => {
   feedback.classList.add('text-danger');
   switch (state.error) {
     case 'url':
-      feedback.textContent = i18n.t('feedback.invalidUrl');
+      feedback.textContent = i18next.t('feedback.invalidUrl');
       break;
     case 'required':
-      feedback.textContent = i18n.t('feedback.invalidRequired');
+      feedback.textContent = i18next.t('feedback.invalidRequired');
       break;
     case 'notOneOf':
-      feedback.textContent = i18n.t('feedback.invalidNotOneOf');
+      feedback.textContent = i18next.t('feedback.invalidNotOneOf');
       break;
     case 'network error':
-      feedback.textContent = i18n.t('feedback.invalidNetwork');
+      feedback.textContent = i18next.t('feedback.invalidNetwork');
       break;
     case 'invalid rss':
-      feedback.textContent = i18n.t('feedback.invalidRSS');
+      feedback.textContent = i18next.t('feedback.invalidRSS');
       break;
     default:
-      feedback.textContent = i18n.t('feedback.invalidUnknown');
+      feedback.textContent = i18next.t('feedback.invalidUnknown');
   }
 };
 
-const formStateHandler = (state, htmlElements, i18n) => {
+const formStateHandler = (state, htmlElements, i18next) => {
   const { input, submit, feedback } = htmlElements;
   switch (state.formState) {
     case 'filling':
@@ -48,57 +48,61 @@ const formStateHandler = (state, htmlElements, i18n) => {
       break;
     case 'sending':
       submit.disabled = true;
-      feedback.textContent = i18n.t('feedback.loading');
+      feedback.textContent = i18next.t('feedback.loading');
       break;
     case 'success':
-      return renderFormFillingSuccess(htmlElements, i18n);
+      return renderFormFillingSuccess(htmlElements, i18next);
     case 'error':
-      return renderFormFillingError(state, htmlElements, i18n);
+      return renderFormFillingError(state, htmlElements, i18next);
     default:
-      throw new Error(`Unknown state: ${state.formState}`);
+      throw new Error(`${i18next.t('errors.unknownState')} ${state.formState}`);
   }
   return state;
 };
 
-const createInnerContainer = (container, i18n) => {
+const createInnerContainer = (container, i18next) => {
   const innerContainer = document.createElement('div');
-  innerContainer.setAttribute('class', 'card border-0');
-  innerContainer.innerHTML = `<div class="card-body"><h2 class="card-title h4">${i18n.t(`${container.id}`)}</h2></div>`;
+  innerContainer.classList.add('card', 'border-0');
+  innerContainer.innerHTML = `<div class="card-body"><h2 class="card-title h4">${i18next.t(`${container.id}`)}</h2></div>`;
   const list = document.createElement('ul');
-  list.setAttribute('class', 'list-group border-0 rounded-0');
+  list.classList.add('list-group', 'border-0', 'rounded-0');
   innerContainer.append(list);
   return container.append(innerContainer);
 };
 
 const createFeedElement = (feed) => {
   const feedElement = document.createElement('li');
-  feedElement.setAttribute('class', 'list-group-item border-0 border-end-0');
+  feedElement.classList.add('list-group-item', 'border-0', 'border-end-0');
   const feedHeader = document.createElement('h3');
   feedHeader.textContent = feed.feedTitle;
-  feedHeader.setAttribute('class', 'h6 m-0');
+  feedHeader.classList.add('h6', 'm-0');
   feedElement.append(feedHeader);
   const feedBody = document.createElement('p');
   feedBody.textContent = feed.feedDescription;
-  feedBody.setAttribute('class', 'm-0 small text-black-50');
+  feedBody.classList.add('m-0', 'small', 'text-black-50');
   feedElement.append(feedBody);
   return feedElement;
 };
 
-const createPostElement = (post, state, i18n) => {
+const createPostElement = (post, state, i18next) => {
   const postElement = document.createElement('li');
-  postElement.setAttribute(
-    'class',
-    `list-group-item d-flex justify-content-between
-     salign-items-start border-0 border-end-0 post-font-size`,
+  postElement.classList.add(
+    'list-group-item',
+    'd-flex',
+    'justify-content-between',
+    'align-items-start',
+    'border-0',
+    'border-end-0',
+    'post-font-size',
   );
 
   const postLink = document.createElement('a');
   postLink.setAttribute('href', `${post.postLink}`);
 
   if (state.visitedPostsID.includes(post.postID)) {
-    postLink.setAttribute('class', 'fw-normal text-muted');
+    postLink.classList.add('fw-normal', 'text-muted');
   } else {
-    postLink.setAttribute('class', 'fw-bold');
+    postLink.classList.add('fw-bold');
   }
 
   postLink.setAttribute('data-id', `${post.postID}`);
@@ -107,17 +111,17 @@ const createPostElement = (post, state, i18n) => {
   postLink.textContent = post.postTitle;
   const postButton = document.createElement('button');
   postButton.setAttribute('type', 'button');
-  postButton.setAttribute('class', 'btn btn-outline-primary btn-sm button-styles');
+  postButton.classList.add('btn', 'btn-outline-primary', 'btn-sm', 'button-styles');
   postButton.setAttribute('data-id', `${post.postID}`);
   postButton.setAttribute('data-bs-toggle', 'modal');
   postButton.setAttribute('data-bs-target', '#modal');
-  postButton.textContent = i18n.t('postButtonRead');
+  postButton.textContent = i18next.t('postButtonRead');
   postElement.append(postLink);
   postElement.append(postButton);
   return postElement;
 };
 
-const readButtonHandler = (state, htmlElements, i18n) => {
+const readButtonHandler = (state, htmlElements, i18next) => {
   const { modal } = htmlElements;
   const modalTitle = modal.querySelector('.modal-title');
   const modalDescription = modal.querySelector('.modal-description');
@@ -127,40 +131,40 @@ const readButtonHandler = (state, htmlElements, i18n) => {
   modalTitle.textContent = currentPost?.postTitle;
   modalDescription.textContent = currentPost?.postDescription;
   modalReadButton.setAttribute('href', `${currentPost?.postLink}`);
-  modalReadButton.textContent = i18n.t('modal.read');
-  modalCloseButton.textContent = i18n.t('modal.close');
+  modalReadButton.textContent = i18next.t('modal.read');
+  modalCloseButton.textContent = i18next.t('modal.close');
   const postElement = document.querySelector(`[data-id="${currentPost?.postID}"]`);
   postElement?.classList.remove('fw-bold');
   postElement?.classList.add('fw-normal');
 };
 
-const renderContent = (state, htmlElements, i18n) => {
+const renderContent = (state, htmlElements, i18next) => {
   const { feedsContainer, postsContainer } = htmlElements;
   feedsContainer.replaceChildren();
   postsContainer.replaceChildren();
   if (state.feeds.length === 0) return state.feeds;
-  createInnerContainer(feedsContainer, i18n);
-  createInnerContainer(postsContainer, i18n);
+  createInnerContainer(feedsContainer, i18next);
+  createInnerContainer(postsContainer, i18next);
   const feedsList = feedsContainer.querySelector('ul');
   const postsList = postsContainer.querySelector('ul');
   state.feeds.forEach((feed) => feedsList.prepend(createFeedElement(feed)));
   state.posts.flat().forEach((post) => {
-    postsList.prepend(createPostElement(post, state, i18n));
+    postsList.prepend(createPostElement(post, state, i18next));
   });
   return state;
 };
 
-export default (state, htmlElements, i18n) => (path) => {
+export default (state, htmlElements, i18next) => (path) => {
   switch (path) {
     case 'formState':
-      return formStateHandler(state, htmlElements, i18n);
+      return formStateHandler(state, htmlElements, i18next);
     case 'feeds':
     case 'posts':
-      return renderContent(state, htmlElements, i18n);
+      return renderContent(state, htmlElements, i18next);
     case 'visitedPostsID':
-      readButtonHandler(state, htmlElements, i18n);
-      return renderContent(state, htmlElements, i18n);
+      readButtonHandler(state, htmlElements, i18next);
+      return renderContent(state, htmlElements, i18next);
     default:
-      throw new Error(`Unknown path: ${path}`);
+      throw new Error(`${i18next.t('errors.unknownPath')} ${path}`);
   }
 };
