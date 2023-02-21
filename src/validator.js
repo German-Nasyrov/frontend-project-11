11ed7fc9f670e003f52c40a5a8ec5746f231cec5
og_gera <german.nasyrov.95@mail.ru>
2023-02-21 11:45:39 +0300

    correct the syntax

--- src/validator.js
+++ src/validator.js
@@ -3,8 +3,7 @@ import * as yup from 'yup';
 const validate = (link, collection) => {
   const schemaStr = yup.string().required().url().trim();
   const schemaMix = yup.mixed().notOneOf([collection]);
-  return schemaStr.validate(link)
-    .then((url) => schemaMix.validate(url));
+  return schemaStr.validate(link).then((url) => schemaMix.validate(url));
 };
 
 export default validate;
