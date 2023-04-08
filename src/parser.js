e9f2612552fb714d722ef6aa46f1cdb15dbdbba2
oggera <german.nasyrov.95@mail.ru>
2023-04-08 18:50:47 +0300

    fix eslint remarks

--- src/parser.js
+++ src/parser.js
@@ -1,21 +1,21 @@
 const replaceHtmlEntites = (string) => {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   div.innerHTML = string;
   return div.textContent || div.innerText;
-}
+};
 
 const parse = (data) => {
   const parser = new DOMParser();
   const xmlDocument = parser.parseFromString(data, 'application/xml');
   const parseErrorElement = xmlDocument.querySelector('parsererror');
   if (parseErrorElement) throw new Error(parseErrorElement.textContent);
-  const title = xmlDocument.querySelector('title').textContent
-  const description = xmlDocument.querySelector('description').textContent
+  const title = xmlDocument.querySelector('title').textContent;
+  const description = xmlDocument.querySelector('description').textContent;
   const postsElements = xmlDocument.querySelectorAll('item');
   const posts = Array.from(postsElements).map((post) => {
     const postTitle = post.querySelector('title').textContent;
     const postDescription = replaceHtmlEntites(post.querySelector('description').textContent);
-    const postLink = post.querySelector('link').textContent
+    const postLink = post.querySelector('link').textContent;
     return { postTitle, postDescription, postLink };
   });
 
